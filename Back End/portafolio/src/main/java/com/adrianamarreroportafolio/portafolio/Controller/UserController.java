0f099fe968036570046f76b0888d5fba8842f59d
adrianamarrero <adrianamarrero235@gmail.com>
2022-07-25 01:00:07 -0600

    Trabajando en el Controller
    Aun no funciona

--- Back End/portafolio/src/main/java/com/adrianamarreroportafolio/portafolio/Controller/UserController.java
+++ Back End/portafolio/src/main/java/com/adrianamarreroportafolio/portafolio/Controller/UserController.java
@@ -7,10 +7,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -33,7 +30,7 @@ public class UserController {
         return iuserService.getUser();
     }
     
-    @PostMapping ("personas/crear")
+    @PostMapping("personas/crear")
     public String createUser(@RequestBody User user){
         iuserService.saveUser(user);
         return "El usuario fue creado correctamente";
--- Back End/portafolio/src/main/java/com/adrianamarreroportafolio/portafolio/PortafolioApplication.java
+++ Back End/portafolio/src/main/java/com/adrianamarreroportafolio/portafolio/PortafolioApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan ({"com.adrianamarreroportafolio.portafolio.Repository.IUserRepository", "com.adrianamarreroportafolio.portafolio.Repository.IEducationRepository", "com.adrianamarreroportafolio.portafolio.Repository.IExperienceRepository", "com.adrianamarreroportafolio.portafolio.Repository.IProjectsRepository", "com.adrianamarreroportafolio.portafolio.Repository.ISkillsRepository"})
-
+//@ComponentScan (basePackages = "com.adrianamarreroportafolio.portafolio.*")//
 public class PortafolioApplication {
 
 	public static void main(String[] args) {
